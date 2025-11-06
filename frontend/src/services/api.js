21f779d66114a60600aca8cfda0150630a96b84c
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2025-11-06 14:47:10 +0600

    env issue fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.API_ENDPOINT || 'http://localhost:8000/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_ENDPOINT || 'http://localhost:8000/api';
+console.log(API_BASE_URL)
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
